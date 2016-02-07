19793d59d75a7e17a04f7ccdc0670394a638682d
廣澤 和成 <hirosawak@eac.ne.jp>
2016-02-07 22:32:45 +0900

    地図の更新

--- js/gmaps.js
+++ js/gmaps.js
@@ -1,7 +1,8 @@
-/*
- * sample-7.js
- * Directions Serviceを利用する | Lopan.jp
- */
+var apiKey = 'AIzaSyDVXUkJfb-QQJHLKH2PixU0tFwshlr4qnU';
+
+var placeIdArray = [];
+var polylines = [];
+var snappedCoordinates = [];
 
 var directionsDisplay;
 var directionsService;
@@ -11,6 +12,8 @@ var timer;
 var center = new google.maps.LatLng(35.681382, 139.766084);
 var start = '東京駅';
 var end = '八王子';
+
+var lat1,lon1;
 
 function initialize(x,y) {
   var mapOptions = {
@@ -43,7 +46,12 @@ function initialize(x,y) {
   map.mapTypes.set('flat', flatType);
   map.setMapTypeId('flat');
 
-  timer = setInterval("exec()",10000);
+  // timer = setInterval("createMarker()",10000);
+
+  runSnapToRoad([
+    new google.maps.LatLng(35.681382, 139.766084),
+    new google.maps.LatLng(35.681345, 139.768888)
+  ])
 }
 
 function calcRoute() {
@@ -63,23 +71,38 @@ function calcRoute() {
   });
 }
 
-func createMarker() {
+function createMarker() {
 
   if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(successCallback,errorCallback);
   }
 
   function successCallback(pos) {
+    if getDistance(lat1,lon1,pos.coords.latitude,pos.coords.longitude) > 200 {
+      lat1 = pos.coords.latitude
+      lat2 = pos.coords.longitude
       var marker1 = new google.maps.Marker({
-          position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);,
+          position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
           map:map
       });
+    }
   }
 
   function errorCallback(error) {
       //失敗のときの処理
       // alert('Geolocationが利用できません');
   }
+}
+
+function getDistance(lat1, lng1, lat2, lng2) {
+       function radians(deg){
+          return deg * Math.PI / 180;
+       }
+       return 6378.14 * Math.acos(Math.cos(radians(lat1))*
+        Math.cos(radians(lat2))*
+        Math.cos(radians(lng2)-radians(lng1))+
+        Math.sin(radians(lat1))*
+        Math.sin(radians(lat2)));
 }
 
 google.maps.event.addDomListener(window, 'load', function() {
@@ -92,9 +115,9 @@ google.maps.event.addDomListener(window, 'load', function() {
   //     alert("Geolocationが利用できません");
   // }
   // function successCallback(pos) {
-  //     var Potition_latitude = pos.coords.latitude;
-  //     var Potition_longitude = pos.coords.longitude;
-  //     // initialize(Potition_latitude,Potition_longitude);
+  //     lat1 = pos.coords.latitude;
+  //     lon1 = pos.coords.longitude;
+  //     // initialize(lat1,lon1);
   //     initialize(35.681382, 139.766084);
   //     calcRoute();
   // }
@@ -103,3 +126,165 @@ google.maps.event.addDomListener(window, 'load', function() {
   //     alert('Geolocationが利用できません');
   // }
 });
+
+
+// function initialize() {
+//   var mapOptions = {
+//     zoom: 17,
+//     center: {lat: -33.8667, lng: 151.1955}
+//   };
+//   map = new google.maps.Map(document.getElementById('map'), mapOptions);
+//
+//   // Adds a Places search box. Searching for a place will center the map on that
+//   // location.
+//   map.controls[google.maps.ControlPosition.RIGHT_TOP].push(
+//       document.getElementById('bar'));
+//   var autocomplete = new google.maps.places.Autocomplete(
+//       document.getElementById('autoc'));
+//   autocomplete.bindTo('bounds', map);
+//   autocomplete.addListener('place_changed', function() {
+//     var place = autocomplete.getPlace();
+//     if (place.geometry.viewport) {
+//       map.fitBounds(place.geometry.viewport);
+//     } else {
+//       map.setCenter(place.geometry.location);
+//       map.setZoom(17);
+//     }
+//   });
+//
+//   // // Enables the polyline drawing control. Click on the map to start drawing a
+//   // // polyline. Each click will add a new vertice. Double-click to stop drawing.
+//   // drawingManager = new google.maps.drawing.DrawingManager({
+//   //   drawingMode: google.maps.drawing.OverlayType.POLYLINE,
+//   //   drawingControl: true,
+//   //   drawingControlOptions: {
+//   //     position: google.maps.ControlPosition.TOP_CENTER,
+//   //     drawingModes: [
+//   //       google.maps.drawing.OverlayType.POLYLINE
+//   //     ]
+//   //   },
+//   //   polylineOptions: {
+//   //     strokeColor: '#696969',
+//   //     strokeWeight: 2
+//   //   }
+//   // });
+//   // drawingManager.setMap(map);
+//
+//   // Snap-to-road when the polyline is completed.
+//   // drawingManager.addListener('polylinecomplete', function(poly) {
+//   //   var path = poly.getPath();
+//   //   polylines.push(poly);
+//   //   placeIdArray = [];
+//   //   runSnapToRoad(path);
+//   // });
+//
+//   // Clear button. Click to remove all polylines.
+//   // $('#clear').click(function(ev) {
+//   //   for (var i = 0; i < polylines.length; ++i) {
+//   //     polylines[i].setMap(null);
+//   //   }
+//   //   polylines = [];
+//   //   ev.preventDefault();
+//   //   return false;
+//   // });
+// }
+
+// Snap a user-created polyline to roads and draw the snapped path
+function runSnapToRoad(path) {
+  var pathValues = [];
+  for (var i = 0; i < path.getLength(); i++) {
+    pathValues.push(path.getAt(i).toUrlValue());
+  }
+
+  $.get('https://roads.googleapis.com/v1/snapToRoads', {
+    interpolate: true,
+    key: apiKey,
+    path: pathValues.join('|')
+  }, function(data) {
+    processSnapToRoadResponse(data);
+    getAndDrawSpeedLimits();
+  });
+}
+
+// Store snapped polyline returned by the snap-to-road method.
+function processSnapToRoadResponse(data) {
+  snappedCoordinates = [];
+  placeIdArray = [];
+  for (var i = 0; i < data.snappedPoints.length; i++) {
+    var latlng = new google.maps.LatLng(
+        data.snappedPoints[i].location.latitude,
+        data.snappedPoints[i].location.longitude);
+    snappedCoordinates.push(latlng);
+    placeIdArray.push(data.snappedPoints[i].placeId);
+  }
+}
+
+// Gets speed limits (for 100 segments at a time) and draws a polyline
+// color-coded by speed limit. Must be called after processing snap-to-road
+// response.
+function getAndDrawSpeedLimits() {
+  for (var i = 0; i <= placeIdArray.length / 100; i++) {
+    // Ensure that no query exceeds the max 100 placeID limit.
+    var start = i * 100;
+    var end = Math.min((i + 1) * 100 - 1, placeIdArray.length);
+
+    drawSpeedLimits(start, end);
+  }
+}
+
+// Gets speed limits for a 100-segment path and draws a polyline color-coded by
+// speed limit. Must be called after processing snap-to-road response.
+function drawSpeedLimits(start, end) {
+    var placeIdQuery = '';
+    for (var i = start; i < end; i++) {
+      placeIdQuery += '&placeId=' + placeIdArray[i];
+    }
+
+    $.get('https://roads.googleapis.com/v1/speedLimits',
+        'key=' + apiKey + placeIdQuery,
+        function(speedData) {
+          processSpeedLimitResponse(speedData, start);
+        }
+    );
+}
+
+// Draw a polyline segment (up to 100 road segments) color-coded by speed limit.
+function processSpeedLimitResponse(speedData, start) {
+  var end = start + speedData.speedLimits.length;
+  for (var i = 0; i < speedData.speedLimits.length - 1; i++) {
+    var speedLimit = speedData.speedLimits[i].speedLimit;
+    var color = getColorForSpeed(speedLimit);
+
+    // Take two points for a single-segment polyline.
+    var coords = snappedCoordinates.slice(start + i, start + i + 2);
+
+    var snappedPolyline = new google.maps.Polyline({
+      path: coords,
+      strokeColor: color,
+      strokeWeight: 6
+    });
+    snappedPolyline.setMap(map);
+    polylines.push(snappedPolyline);
+  }
+}
+
+function getColorForSpeed(speed_kph) {
+  if (speed_kph <= 40) {
+    return 'purple';
+  }
+  if (speed_kph <= 50) {
+    return 'blue';
+  }
+  if (speed_kph <= 60) {
+    return 'green';
+  }
+  if (speed_kph <= 80) {
+    return 'yellow';
+  }
+  if (speed_kph <= 100) {
+    return 'orange';
+  }
+  return 'red';
+}
+
+$(window).load(initialize);
